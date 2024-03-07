b6563a9b6646c8f358a779cce2f1181885d00769
Xave Perry <xaveperry@gmail.com>
2024-03-06 19:54:14 -0700

    many finished projects

--- Untitled-1.py
+++ Untitled-1.py
@@ -1,16 +1,1 @@
-#%%
-import plotly.express as px
-data = px.data.iris()
-data.head()
-
-#%%
-px.scatter(data,
-    x="sepal_width", 
-    y="sepal_length", 
-    color = "species", 
-    size = "petal_length",
-    symbol = "species",
-    hover_data=["species", "petal_width"], 
-    title = "The relationship between sepal width and sepal length"
-)
-# %%
+print('hello world')
